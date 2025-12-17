37bbc8aac75469729720648b3beb0c4777c23b24
blum99 <blum99@gmaile.com>
2025-12-17 08:52:15 +0100

    Finished versioning cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,7 @@
 'use client';
 
-import { ChatApp as ChatAppV1 } from "./components/chat/v1";
-import { ChatApp as ChatAppV2 } from "./components/chat/v2";
-
-const frontendConfig = require('../../frontend.config.js');
+import { ChatApp } from "./components/chat/ChatApp";
 
 export default function Home() {
-  // Select version from config file (frontend.config.js)
-  const FRONTEND_VERSION = frontendConfig.FRONTEND_VERSION || 'v2';
-  
-  // Version selector
-  const versions = {
-    v1: ChatAppV1,
-    v2: ChatAppV2,
-  };
-
-  const SelectedChatApp = versions[FRONTEND_VERSION as keyof typeof versions] || ChatAppV2;
-  
-  return <SelectedChatApp />;
+  return <ChatApp />;
 }
